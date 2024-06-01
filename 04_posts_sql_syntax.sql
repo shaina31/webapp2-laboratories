73d052fc1f595c2559a65d55f51ae0d913f0267b
shaina31 <80665132+shaina31@users.noreply.github.com>
2024-06-01 22:31:39 +0800

    Update 04_posts_sql_syntax.sql

--- 04_posts_sql_syntax.sql
+++ 04_posts_sql_syntax.sql
@@ -11,6 +11,8 @@ CREATE TABLE posts(
     FOREIGN KEY (user_id) REFERENCES users(id)
     );
 
+-- - values of the posts table are as follows:
+
 INSERT INTO posts (user_id, title, body) VALUES (1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"),
 (1, "qui est esse", "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"),
 (1, "ea molestias quasi exercitationem repellat qui ipsa sit aut", "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"),
